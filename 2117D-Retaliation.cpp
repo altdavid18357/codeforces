229780d00e9844b703f7bfd15b156045e27731c6
altdavid18357 <liudavid18357@gmail.com>
2025-07-17 16:05:44 -0500

    Update 2117D-Retaliation.cpp

--- 2117D-Retaliation.cpp
+++ 2117D-Retaliation.cpp
@@ -3,6 +3,15 @@
 #include <vector>
 using namespace std;
 
+/*
+essentially, the main idea here is that 
+#1 - the array has to be an arithmetic seqeunce, which is what the first part is doing. this is kind of like pruning in a way.
+#2 - basically we want all elements of the array to be divisible by (n+1) --> which is basically when you use operation 1 and operation 2 at the same time and thus the array is explodable.
+in order to get the array to that position, there could be a n number of extra operations of 1 or 2. 
+We use the idea that when the difference is positive, we should undo operation 1 whereas if the difference is negative we should undo operation 2.
+lastly, we check if we have any negatives and the first part of step 2 --> answer
+
+*/
 
 void solve() {
 	int n;
